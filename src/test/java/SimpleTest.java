b660c4808c0f89e0b64225aa48a360957c558623
Igor Shmagrinskiy <unrealwork@gmail.com>
2022-01-01 01:31:17 +0300

    Initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
