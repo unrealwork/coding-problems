629cd312c9da5558f8a22fc16c448f682800e0cb
unrealwork <unrealwork@gmail.com>
2022-01-02 02:47:38 +0300

    leetcode: Increasing Triplet Subsequence: not implemented

--- src/main/java/com/leetcode/array/IncreasingTripletSubsequence.java
+++ src/main/java/com/leetcode/array/IncreasingTripletSubsequence.java
@@ -1,11 +1,13 @@
 package com.leetcode.array;
 
+
+
 public class IncreasingTripletSubsequence {
     private IncreasingTripletSubsequence() {
         
     }
 
     public static boolean increasingTriplet(int[] nums) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 }
