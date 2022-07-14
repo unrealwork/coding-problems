56d49ba7c3f17dea15e7bb34086735eb8b351066
Igor Shmagrinskiy <igor.shmagrinsky@axibase.com>
2022-07-14 10:33:11 +0300

    leetcode: Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/java/com/leetcode/tree/ConstructBinaryTree.java
+++ src/main/java/com/leetcode/tree/ConstructBinaryTree.java
@@ -1,18 +1,25 @@
 package com.leetcode.tree;
 
-import java.util.Objects;
-
 /**
  * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
  */
 final class ConstructBinaryTree {
     private ConstructBinaryTree() {
-        
     }
 
+
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
-        Objects.requireNonNull(preorder);
-        Objects.requireNonNull(inorder);
-        throw new UnsupportedOperationException();
+        return buildTreeRec(preorder, inorder, 0, preorder.length - 1, 0);
+    }
+
+    private static TreeNode buildTreeRec(int[] preorder, int[] inorder, int left, int right, int index) {
+        for (int i = left; i <= right; i++) {
+            if (inorder[i] == preorder[index]) {
+                TreeNode leftNode = buildTreeRec(preorder, inorder, left, i - 1, index + 1);
+                TreeNode rightNode = buildTreeRec(preorder, inorder, i + 1, right, index + (i - left) + 1);
+                return new TreeNode(inorder[i], leftNode, rightNode);
+            }
+        }
+        return null;
     }
 }
--- src/test/java/com/leetcode/tree/ConstructBinaryTreeTest.java
+++ src/test/java/com/leetcode/tree/ConstructBinaryTreeTest.java
@@ -1,16 +1,26 @@
 package com.leetcode.tree;
 
+import com.leetcode.array.IntArrayConverter;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConstructBinaryTreeTest {
 
-    @Test
-    @DisplayName("Empty traverse should generate empty tree")
-    void buildTree() {
-        TreeNode res = ConstructBinaryTree.buildTree(new int[] {}, new int[] {});
-        assertNull(res);
+    @ParameterizedTest
+    @CsvSource( {
+            "'', '', ''",
+            "'3', '3', '3'",
+            "'3,9,20,15,7', '9,3,15,20,7', '3,9,20,null,null,15,7'"
+    })
+    @DisplayName("buildTree should construct correct binary tree")
+    void buildTree(@ConvertWith(IntArrayConverter.class) int[] preorder,
+                   @ConvertWith(IntArrayConverter.class) int[] inorder,
+                   @ConvertWith(TreeNodeConverter.class) TreeNode node) {
+        TreeNode res = ConstructBinaryTree.buildTree(preorder, inorder);
+        assertEquals(res, node);
     }
 }
